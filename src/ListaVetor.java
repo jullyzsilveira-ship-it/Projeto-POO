92d25bb663ab30e17feff55b3e4337c99bdc4b75
JullyZSilveira <jullyzsilveira@gmail.com>
2026-06-20 21:16:01 -0300

    Inseri a logica dos metodos que faltavam na classe ListaVetor e criei um outro metodo para validar posiçao para ser utilizado em T get, T remove

--- src/ListaVetor.java
+++ src/ListaVetor.java
@@ -8,15 +8,20 @@ public class ListaVetor <T> implements Lista <T>{
         this.tamanho = 0;
     }
 
-    private void validaPosInserida(int pos){
+    private void validaPosInsercao(int pos){
         if (pos < 0 || pos > this.tamanho)
+            throw new IllegalArgumentException("Posição da lista inválida!");
+    }
+
+    private void validaPos(int pos){
+        if (pos < 0 || pos >= this.tamanho)
             throw new IllegalArgumentException("Posição da lista inválida!");
     }
 
 
     @Override
     public void add(T valor, int pos) {
-        validaPosInserida(pos);
+        validaPosInsercao(pos);
 
         for (int i = this.tamanho; i > pos; i--){
             this.itens[i] = this.itens[i - 1];
@@ -28,26 +33,46 @@ public class ListaVetor <T> implements Lista <T>{
 
     @Override
     public void add(T valor) {
-
+        add(valor,this.tamanho);
     }
 
     @Override
     public T get(int pos) {
-        return null;
+        validaPos(pos);
+        return this.itens[pos];
     }
 
     @Override
     public T remove(int pos) {
-        return null;
+        validaPos(pos);
+
+        T removido = this.itens[pos];
+
+        for (int i = pos; i < this.tamanho -1; i++){
+            this.itens[i] = this.itens[i + 1];
+        }
+
+        this.tamanho--;
+        this.itens[this.tamanho] = new;
+
+        return removido;
     }
 
     @Override
     public boolean remove(T valor) {
+
+        for (int i = 0; i < this.tamanho; i++){
+            if (this.itens[i].equals(valor)){
+                remove(i);
+
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.tamanho;
     }
 }
